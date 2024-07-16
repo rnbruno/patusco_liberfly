8cc9d1c7b5b78c04f373edae586d95dfc565652e
rnbruno <brunosmatias@gmail.com>
2024-07-16 17:30:02 -0300

    1607

--- resources/js/app.js
+++ resources/js/app.js
@@ -68,19 +68,19 @@ import router from './routes/index'
 import VueSweetalert2 from "vue-sweetalert2";
 import useAuth from "./Pages/composables/auth";
 import { abilitiesPlugin } from '@casl/vue';
-import ability from './Pages/services/ability';
+// import ability from './Pages/services/ability';
 // import Select2 from 'vue3-select2-component';
 
 
 const app = createApp({
-    setup() {
-        const { getUser } = useAuth()
-        onMounted(getUser)
-    }
+    // setup() {
+    //     const { getUser } = useAuth()
+    //     onMounted(getUser)
+    // }
 })
 app.use(router)
 app.use(VueSweetalert2)
-app.use(abilitiesPlugin, ability)
+// app.use(abilitiesPlugin, ability)
 app.component('Pagination', LaravelVuePagination)
 // app.component('Select2', Select2)
 app.mount('#app')
--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -9,57 +9,66 @@ import PostsEdit from '../Pages/components/Posts/Edit.vue'
 import Usuarios from '../Pages/Usuario/Usuarios.vue'
 import Login from '../Pages/components/Login.vue'
 
-function auth(to, from, next) {
-    if (JSON.parse(localStorage.getItem('loggedIn'))) {
-        next()
-    }
+ const Home = { template: "<div>Home</div>"};
+ const About = { template: "<div>About</div>"};
+ const Product = { template: "<div>Product</div>"};
+// function auth(to, from, next) {
+//     if (JSON.parse(localStorage.getItem('loggedIn'))) {
+//         next()
+//     }
 
-    next('/login')
-}
+//     next('/login')
+// }
+
+// const routes = [
+//     {
+//         path: '/',
+//         redirect: { name: 'login' },
+//         component: GuestLayout,
+//         children: [
+//             {
+//                 path: '/login',
+//                 name: 'login',
+//                 component: Login
+//             },
+//         ]
+//     },
+//     {
+//         path: '/Usuarios',
+//         redirect: { name: 'usuarios' },
+//         component: Usuarios,
+        
+//     },
+//     {
+//         component: AuthenticatedLayout,
+//         beforeEnter: auth,
+//         children: [
+//             {
+//                 path: '/posts',
+//                 name: 'posts.index',
+//                 component: PostsIndex,
+//                 meta: { title: 'Posts' }
+//             },
+//             {
+//                 path: '/posts/create',
+//                 name: 'posts.create',
+//                 component: PostsCreate,
+//                 meta: { title: 'Add new post' }
+//             },
+//             {
+//                 path: '/posts/edit/:id',
+//                 name: 'posts.edit',
+//                 component: PostsEdit,
+//                 meta: { title: 'Edit post' }
+//             },
+//         ]
+//     }
+// ]
 
 const routes = [
-    {
-        path: '/',
-        redirect: { name: 'login' },
-        component: GuestLayout,
-        children: [
-            {
-                path: '/login',
-                name: 'login',
-                component: Login
-            },
-        ]
-    },
-    {
-        path: '/Usuarios',
-        redirect: { name: 'usuarios' },
-        component: Usuarios,
-        
-    },
-    {
-        component: AuthenticatedLayout,
-        beforeEnter: auth,
-        children: [
-            {
-                path: '/posts',
-                name: 'posts.index',
-                component: PostsIndex,
-                meta: { title: 'Posts' }
-            },
-            {
-                path: '/posts/create',
-                name: 'posts.create',
-                component: PostsCreate,
-                meta: { title: 'Add new post' }
-            },
-            {
-                path: '/posts/edit/:id',
-                name: 'posts.edit',
-                component: PostsEdit,
-                meta: { title: 'Edit post' }
-            },
-        ]
-    }
+    { path: "/", component: Home },
+    { path: "/about", component: About},
+    { path: "/product", component: Product}
 ]
 
 export default createRouter({
